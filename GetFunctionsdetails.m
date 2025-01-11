bbc9c193d460947381c041ff98094d7651772f7b
zebka <a.falahat@outlook.com>
2025-01-11 23:49:27 +0330

    fix some issues

--- GetFunctionsdetails.m
+++ GetFunctionsdetails.m
@@ -1,138 +1,138 @@
 function [lb,ub,dim,fobj] = GetFunctionsdetails(F,n1)
     switch F
         case 'BF1'
-            fobj = @F1;
+            fobj = @BF1;
             lb=-100;
             ub=100;
             dim=n1;
             
         case 'BF2'
-            fobj = @F2;
+            fobj = @BF2;
             lb=-10;
             ub=10;
             dim=n1;
             
         case 'BF3'
-            fobj = @F3;
+            fobj = @BF3;
             lb=-100;
             ub=100;
             dim=n1;
             
         case 'BF4'
-            fobj = @F4;
+            fobj = @BF4;
             lb=-100;
             ub=100;
             dim=n1;
             
         case 'BF5'
-            fobj = @F5;
+            fobj = @BF5;
             lb=-30;
             ub=30;
             dim=n1;
             
         case 'BF6'
-            fobj = @F6;
+            fobj = @BF6;
             lb=-100;
             ub=100;
             dim=n1;
             
         case 'BF7'
-            fobj = @F7;
+            fobj = @BF7;
             lb=-1.28;
             ub=1.28;
             dim=n1;
             
         case 'BF8'
-            fobj = @F8;
+            fobj = @BF8;
             lb=-500;
             ub=500;
             dim=n1;
             
         case 'BF9'
-            fobj = @F9;
+            fobj = @BF9;
             lb=-5.12;
             ub=5.12;
             dim=n1;
             
         case 'BF10'
-            fobj = @F10;
+            fobj = @BF10;
             lb=-32;
             ub=32;
             dim=n1;
         case 'BF11'
-            fobj = @F11;
+            fobj = @BF11;
             lb=-600;
             ub=600;
             dim=n1;
             
         case 'BF12'
-            fobj = @F12;
+            fobj = @BF12;
             lb=-50;
             ub=50;
             dim=n1;
             
         case 'BF13'
-            fobj = @F13;
+            fobj = @BF13;
             lb=-50;
             ub=50;
             dim=n1;
 
         case 'BF14'
-            fobj = @F14;
+            fobj = @BF14;
             lb=-65.536;
             ub=65.536;
             dim=2;
             
         case 'BF15'
-            fobj = @F15;
+            fobj = @BF15;
             lb=-5;
             ub=5;
             dim=4;
             
         case 'BF16'
-            fobj = @F16;
+            fobj = @BF16;
             lb=-5;
             ub=5;
             dim=2;
             
         case 'BF17'
-            fobj = @F17;
+            fobj = @BF17;
             lb=[-5,0];
             ub=[10,15];
             dim=2;
             
         case 'BF18'
-            fobj = @F18;
+            fobj = @BF18;
             lb=-5;
             ub=5;
             dim=2;
             
         case 'BF19'
-            fobj = @F19;
+            fobj = @BF19;
             lb=0;
             ub=1;
             dim=3;
             
         case 'BF20'
-            fobj = @F20;
+            fobj = @BF20;
             lb=0;
             ub=1;
             dim=6;     
             
         case 'BF21'
-            fobj = @F21;
+            fobj = @BF21;
             lb=0;
             ub=10;
             dim=4;    
 
         case 'BF22'
-            fobj = @F22;
+            fobj = @BF22;
             lb=0;
             ub=10;
             dim=4;    
             
         case 'F23'
-            fobj = @F23;
+            fobj = @BF23;
             lb=0;
             ub=10;
             dim=4;
@@ -348,15 +348,15 @@ function [lb,ub,dim,fobj] = GetFunctionsdetails(F,n1)
     end
 end
 % F1
-function o = F1(x)
+function o = BF1(x)
 o=sum(x.^2);
 end
 % F2
-function o = F2(x)
+function o = BF2(x)
 o=sum(abs(x))+prod(abs(x));
 end
 % F3
-function o = F3(x)
+function o = BF3(x)
 dim=size(x,2);
 o=0;
 for i=1:dim
@@ -364,56 +364,56 @@ for i=1:dim
 end
 end
 % F4
-function o = F4(x)
+function o = BF4(x)
 o=max(abs(x));
 end
 % F5
-function o = F5(x)
+function o = BF5(x)
 dim=size(x,2);
 o=sum(100*(x(2:dim)-(x(1:dim-1).^2)).^2+(x(1:dim-1)-1).^2);
 end
 % F6
-function o = F6(x)
+function o = BF6(x)
 o=sum(abs((x+.5)).^2);
 end
 % F7
-function o = F7(x)
+function o = BF7(x)
 dim=size(x,2);
 o=sum([1:dim].*(x.^4))+rand;
 end
 % F8
-function o = F8(x)
+function o = BF8(x)
 o=sum(-x.*sin(sqrt(abs(x))));
 end
 % F9
-function o = F9(x)
+function o = BF9(x)
 dim=size(x,2);
 o=sum(x.^2-10*cos(2*pi.*x))+10*dim;
 end
 % F10
-function o = F10(x)
+function o = BF10(x)
 dim=size(x,2);
 o=-20*exp(-.2*sqrt(sum(x.^2)/dim))-exp(sum(cos(2*pi.*x))/dim)+20+exp(1);
 end
 % F11
-function o = F11(x)
+function o = BF11(x)
 dim=size(x,2);
 o=sum(x.^2)/4000-prod(cos(x./sqrt([1:dim])))+1;
 end
 % F12
-function o = F12(x)
+function o = BF12(x)
 dim=size(x,2);
 o=(pi/dim)*(10*((sin(pi*(1+(x(1)+1)/4)))^2)+sum((((x(1:dim-1)+1)./4).^2).*...
 (1+10.*((sin(pi.*(1+(x(2:dim)+1)./4)))).^2))+((x(dim)+1)/4)^2)+sum(Ufun(x,10,100,4));
 end
 % F13
-function o = F13(x)
+function o = BF13(x)
 dim=size(x,2);
 o=.1*((sin(3*pi*x(1)))^2+sum((x(1:dim-1)-1).^2.*(1+(sin(3.*pi.*x(2:dim))).^2))+...
 ((x(dim)-1)^2)*(1+(sin(2*pi*x(dim)))^2))+sum(Ufun(x,5,100,4));
 end
 % F14
-function o = F14(x)
+function o = BF14(x)
 aS=[-32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32 -32 -16 0 16 32;...
 -32 -32 -32 -32 -32 -16 -16 -16 -16 -16 0 0 0 0 0 16 16 16 16 16 32 32 32 32 32];
 for j=1:25
@@ -422,26 +422,26 @@ end
 o=(1/500+sum(1./([1:25]+bS))).^(-1);
 end
 % F15
-function o = F15(x)
+function o = BF15(x)
 aK=[.1957 .1947 .1735 .16 .0844 .0627 .0456 .0342 .0323 .0235 .0246];
 bK=[.25 .5 1 2 4 6 8 10 12 14 16];bK=1./bK;
 o=sum((aK-((x(1).*(bK.^2+x(2).*bK))./(bK.^2+x(3).*bK+x(4)))).^2);
 end
 % F16
-function o = F16(x)
+function o = BF16(x)
 o=4*(x(1)^2)-2.1*(x(1)^4)+(x(1)^6)/3+x(1)*x(2)-4*(x(2)^2)+4*(x(2)^4);
 end
 % F17
-function o = F17(x)
+function o = BF17(x)
 o=(x(2)-(x(1)^2)*5.1/(4*(pi^2))+5/pi*x(1)-6)^2+10*(1-1/(8*pi))*cos(x(1))+10;
 end
 % F18
-function o = F18(x)
+function o = BF18(x)
 o=(1+(x(1)+x(2)+1)^2*(19-14*x(1)+3*(x(1)^2)-14*x(2)+6*x(1)*x(2)+3*x(2)^2))*...
     (30+(2*x(1)-3*x(2))^2*(18-32*x(1)+12*(x(1)^2)+48*x(2)-36*x(1)*x(2)+27*(x(2)^2)));
 end
 % F19
-function o = F19(x)
+function o = BF19(x)
 aH=[3 10 30;.1 10 35;3 10 30;.1 10 35];cH=[1 1.2 3 3.2];
 pH=[.3689 .117 .2673;.4699 .4387 .747;.1091 .8732 .5547;.03815 .5743 .8828];
 o=0;
@@ -450,7 +450,7 @@ for i=1:4
 end
 end
 % F20
-function o = F20(x)
+function o = BF20(x)
 aH=[10 3 17 3.5 1.7 8;.05 10 17 .1 8 14;3 3.5 1.7 10 17 8;17 8 .05 10 .1 14];
 cH=[1 1.2 3 3.2];
 pH=[.1312 .1696 .5569 .0124 .8283 .5886;.2329 .4135 .8307 .3736 .1004 .9991;...
@@ -461,7 +461,7 @@ for i=1:4
 end
 end
 % F21
-function o = F21(x)
+function o = BF21(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
 cSH=[.1 .2 .2 .4 .4 .6 .3 .7 .5 .5];
 o=0;
@@ -470,7 +470,7 @@ for i=1:5
 end
 end
 % F22
-function o = F22(x)
+function o = BF22(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
 cSH=[.1 .2 .2 .4 .4 .6 .3 .7 .5 .5];
 o=0;
@@ -479,7 +479,7 @@ for i=1:7
 end
 end
 % F23
-function o = F23(x)
+function o = BF23(x)
 aSH=[4 4 4 4;1 1 1 1;8 8 8 8;6 6 6 6;3 7 3 7;2 9 2 9;5 5 3 3;8 1 8 1;6 2 6 2;7 3.6 7 3.6];
 cSH=[.1 .2 .2 .4 .4 .6 .3 .7 .5 .5];
 o=0;
